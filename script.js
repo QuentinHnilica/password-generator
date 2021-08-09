a132ecbd72a94569c7b0276627e95f1d2213e322
unknown <Quentinhnilica@gmail.com>
2021-08-09 13:40:28 -0700

    Final Push, Everything is looking good

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ var conditions = []
 
 function makePassword(){
     var newCondition //this is how we will tell which char to use
+    ourPassword = [] //clear the array so you can change the size of the password to a lower amt
     for(var i = 0; i < realLength; i++){
         newCondition = conditions[Math.floor(Math.random() * conditions.length)]
-        console.log(newCondition)
         
         //Before Giving a value, make sure we are usuing all conditions no matter
         //what.
@@ -33,48 +33,56 @@ function makePassword(){
         }
         
     }
-        var finalPass = ourPassword.join()
-        document.getElementById("password").innerHTML = finalPass.replace(/,/g," ") 
+        var finalPass = ourPassword.join() //converts array to string
+        document.getElementById("password").innerHTML = finalPass.replace(/,/g," ") //displays pass without commas
         document.getElementById("myButton").innerHTML = "Generate Again"
 }
 
 function startGenerating(){
-    var getLength = window.prompt("Choose characters, Max 128"); //gets size min of 8
-    realLength = parseInt(getLength); //converts string to int
-    //Just in case the player tries some funny business
+    var getLength = document.querySelector('#fname'); //gets size min of 8
+    const regex = /\d/;
+    const doesItHaveNumber = regex.test(getLength.value) //checks string for a number
 
-    conditions = []; //Reset Peramiter 
-    if (realLength < 8){
-        realLength = 8;
-    }
-    else if (realLength > 128){
-        realLength = 128;
-    }
 
-    //Bools for special chars
-    includeLowerCase = window.confirm("Do you want lowercase Letters?");
-    includeUpperCase = window.confirm("Do you want UPPERCASE?");
-    includeNum = window.confirm("Do you want Numbers?");
-    includeSpecial = window.confirm("Do you want Special Characters? (IE. !@#$%^&*)");
-
-    if (includeLowerCase){
-        conditions.push("lower")
-    }
-    if (includeUpperCase){
-        conditions.push("upper")
-    }
-    if (includeNum){
-        conditions.push("num")
-    }
-    if (includeSpecial){
-        conditions.push("special")
-    }
-
-    if (conditions.length > 0){
-        console.log(conditions)
-        makePassword();
-    }
+    if (doesItHaveNumber){ //to ensure they are inputting correctly
 
     
+        realLength = getLength.value.replace(/\D/g,''); //converts string to int
 
+        conditions = []; //Reset Peramiter 
+        if (realLength < 8){ //in case they put a number lower or higher than allowed amt
+            realLength = 8;
+        }
+        else if (realLength > 128){
+            realLength = 128;
+        }
+
+        //Bools for special chars
+        includeLowerCase = document.querySelector('#lowerCase:checked') !== null;
+        includeUpperCase = document.querySelector('#upperCase:checked') !== null;
+        includeNum = document.querySelector('#num:checked') !== null;
+        includeSpecial = document.querySelector('#special:checked') !== null;
+
+        if (includeLowerCase){
+            conditions.push("lower")
+        }
+        if (includeUpperCase){
+            conditions.push("upper")
+        }
+        if (includeNum){
+            conditions.push("num")
+        }
+        if (includeSpecial){
+            conditions.push("special")
+        }
+
+        if (conditions.length > 0){
+            makePassword();
+        }
+        else
+        document.getElementById("password").innerHTML = "You must check at least 1 box" //To Guide user of rules
+    }
+    else{
+        document.getElementById("password").innerHTML = "Input A Number" //To guide user of rules
+    }
 }
